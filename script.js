62cd85c197aa8a9ad7c33d4657649b1d257997d6
Marco Pauws <mpauws@gmail.com>
2020-07-14 13:54:54 +0200

    update folder

--- script.js
+++ script.js
@@ -1,84 +1,75 @@
 const moviesRecent = movieDatabase.Movies.filter((item) => {
-    return item.Year >= 2014
-})
+  return item.Year >= 2014;
+});
 
 // {{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
-// {{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
-// Onderstaande filters moeten nog korter kunnen
+// Onderstaande filters moeten nog korter kunnen!!!!
 
 const moviesAvengers = movieDatabase.Movies.filter((item) => {
-    return item.Title.includes("Avengers")
-})
+  return item.Title.includes("Avengers");
+});
 
 const moviesXmen = movieDatabase.Movies.filter((item) => {
-    return item.Title.includes("X-Men")
-})
+  return item.Title.includes("X-Men");
+});
 
 const moviesPrincess = movieDatabase.Movies.filter((item) => {
-    return item.Title.includes("Princess")
-})
+  return item.Title.includes("Princess");
+});
 
 const moviesBatman = movieDatabase.Movies.filter((item) => {
-    return item.Title.includes("Batman")
-})
+  return item.Title.includes("Batman");
+});
 
-// {{{{{{{{{{{{}}}}}}}}}}}}
-// Kortere manier van Bram:
-
-/* 
-const filterMovieByTitle = function (titlepart) {
-    const moviesWithTitlePart = allMovies.filter(movie => movie.Title.includes(titlepart));
-    addMoviesToDom(moviesWithcTitlePart);
-} */
-
-// {{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
 // {{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
 
 const addMoviesToDom = (whateverfilter) => {
-    const htmlMovieContainer = document.getElementById("movie-html-container-id");
-    htmlMovieContainer.innerHTML = "";
+  const htmlMovieContainer = document.getElementById("movie-html-container-id");
+  htmlMovieContainer.innerHTML = "";
 
-    for (let i = 0; i < whateverfilter.length; i++) {
-        const newLi = document.createElement('li');
-        const newImg = document.createElement('img');
-        newLi.classList.add("movie-html-container-item");
-        const newAElement = document.createElement('a');
-        newImg.classList.add("movie-poster");
-        newLi.appendChild(newAElement);
-        document.getElementsByClassName("movie-poster")
-        newAElement.appendChild(newImg).src;
-        newAElement.href = "https://www.imdb.com/title/" + whateverfilter.imdbID;
-        htmlMovieContainer.appendChild(newLi);
-        newImg.src = whateverfilter[i].Poster;
-    }
-}
+  for (let i = 0; i < whateverfilter.length; i++) {
+    const newLi = document.createElement("li");
+    const newImg = document.createElement("img");
+    newLi.classList.add("movie-html-container-item");
+    const newAElement = document.createElement("a");
+    newImg.classList.add("movie-poster");
+    newLi.appendChild(newAElement);
+    document.getElementsByClassName("movie-poster");
+    newAElement.appendChild(newImg).src;
+    newAElement.href = "https://www.imdb.com/title/" + whateverfilter[i].imdbID;
+    htmlMovieContainer.appendChild(newLi);
+    newImg.src = whateverfilter[i].Poster;
+  }
+};
+
+// For each van maken ^
+
+// All list dmv addMoviesToDom aanroepen
 
 const allRadioButtons = document.getElementsByName("filter-buttons");
 
 allRadioButtons.forEach((filterButton) => {
-    filterButton.addEventListener("change", (event) => {
-        // let handleOnChangeEvent = event.target.value;
+  filterButton.addEventListener("change", (event) => {
+    // let handleOnChangeEvent = event.target.value;
 
-        switch (event.target.value) {
-            case 'recent-movies':
-                addMoviesToDom(moviesRecent);
-                break;
-            case 'avengers-movies':
-                addMoviesToDom(moviesAvengers);
-                break;
-            case 'xmen-movies':
-                addMoviesToDom(moviesXmen);
-                break;
-            case 'princess-movies':
-                addMoviesToDom(moviesPrincess);
-                break;
-            case 'batman-movies':
-                addMoviesToDom(moviesBatman);;
-                break;
-            default:
-                console.log("Ehmm...")
-        }
+    switch (event.target.value) {
+      case "recent-movies":
+        addMoviesToDom(moviesRecent);
+        break;
+      case "avengers-movies":
+        addMoviesToDom(moviesAvengers);
+        break;
+      case "xmen-movies":
+        addMoviesToDom(moviesXmen);
+        break;
+      case "princess-movies":
+        addMoviesToDom(moviesPrincess);
+        break;
+      case "batman-movies":
+        addMoviesToDom(moviesBatman);
+        break;
+      default:
+        console.log("Ehmm...");
     }
-    )
-}
-)
+  });
+});
