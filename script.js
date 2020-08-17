780eb6714736070568ba228f24c34fdd33f40cde
Marco Pauws <mpauws@gmail.com>
2020-08-17 13:18:56 +0200

    CSS styling and JS refactoring

--- script.js
+++ script.js
@@ -1,75 +1,116 @@
+// {{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}}}}
+// {{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}}}}
+// SVG EXPERIMENT
+// {{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}}}}
+// {{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}}}}
+
+const svgStarIcon =
+   '<svg width="15" height="15" xmlns="http://www.w3.org/2000/svg" class="star-icon" viewBox="0 0 24 24" fill="currentColor" role="presentation"><path d="M12 20.1l5.82 3.682c1.066.675 2.37-.322 2.09-1.584l-1.543-6.926 5.146-4.667c.94-.85.435-2.465-.799-2.567l-6.773-.602L13.29.89a1.38 1.38 0 0 0-2.581 0l-2.65 6.53-6.774.602C.052 8.126-.453 9.74.486 10.59l5.147 4.666-1.542 6.926c-.28 1.262 1.023 2.26 2.09 1.585L12 20.099z"></path></svg>';
+
+const allMovies = movieDatabase.Movies;
+
 const moviesRecent = movieDatabase.Movies.filter((item) => {
-  return item.Year >= 2014;
+   return item.Year >= 2014;
 });
 
-// {{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
-// Onderstaande filters moeten nog korter kunnen!!!!
-
-const moviesAvengers = movieDatabase.Movies.filter((item) => {
-  return item.Title.includes("Avengers");
-});
-
-const moviesXmen = movieDatabase.Movies.filter((item) => {
-  return item.Title.includes("X-Men");
-});
-
-const moviesPrincess = movieDatabase.Movies.filter((item) => {
-  return item.Title.includes("Princess");
-});
-
-const moviesBatman = movieDatabase.Movies.filter((item) => {
-  return item.Title.includes("Batman");
-});
-
-// {{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
-
-const addMoviesToDom = (whateverfilter) => {
-  const htmlMovieContainer = document.getElementById("movie-html-container-id");
-  htmlMovieContainer.innerHTML = "";
-
-  for (let i = 0; i < whateverfilter.length; i++) {
-    const newLi = document.createElement("li");
-    const newImg = document.createElement("img");
-    newLi.classList.add("movie-html-container-item");
-    const newAElement = document.createElement("a");
-    newImg.classList.add("movie-poster");
-    newLi.appendChild(newAElement);
-    document.getElementsByClassName("movie-poster");
-    newAElement.appendChild(newImg).src;
-    newAElement.href = "https://www.imdb.com/title/" + whateverfilter[i].imdbID;
-    htmlMovieContainer.appendChild(newLi);
-    newImg.src = whateverfilter[i].Poster;
-  }
+const movieFilter = (filterCategory) => {
+   const movieTitlesWithWord = movieDatabase.Movies.filter((movie) => {
+      return movie.Title.includes(filterCategory);
+   }).map((movies) => {
+      return movies;
+   });
+   addMoviesToDom(movieTitlesWithWord, filterCategory);
 };
 
-// For each van maken ^
+const addMoviesToDom = (whateverfilter, titleDisplay) => {
+   const htmlMovieContainer = document.getElementById("movie-html-container-id");
+   htmlMovieContainer.innerHTML = "";
 
-// All list dmv addMoviesToDom aanroepen
+   document.querySelector(".subtitle").innerHTML = titleDisplay;
+
+   whateverfilter.forEach((element) => {
+      // Create new list item element
+      const newLi = document.createElement("li");
+
+      // Create new img element
+      const newImg = document.createElement("img");
+
+      // Create new a element
+      const newAElement = document.createElement("a");
+
+      // Create new a element
+      const newTitleAElement = document.createElement("a");
+
+      // Create new h4 element
+      const newH4Element = document.createElement("h4");
+
+      // Create new div element
+      const newRatingDiv = document.createElement("div");
+
+      // Add class to list item element
+      newLi.classList.add("movie-html-container-item");
+
+      // Add class to img element
+      newImg.classList.add("movie-poster");
+
+      // Add new a element to new list item
+      newLi.appendChild(newAElement).target;
+
+      // Create src attribute in new img element
+      newAElement.appendChild(newImg).src;
+
+      // Set href attribute to movieposter jpg location
+      newAElement.href = "https://www.imdb.com/title/" + element.imdbID;
+
+      // Add new list item element to the html movie container
+      htmlMovieContainer.appendChild(newLi);
+
+      // Set src attribute of new img to the poster jpg path
+      newImg.src = element.Poster;
+
+      // Set target attribute of new img to "_blank"
+      newAElement.target = '"_blank"';
+
+      // Set h4 value to element title
+      newH4Element.innerHTML = `${element.Title} (${element.Year})`;
+
+      newRatingDiv.innerHTML = `${svgStarIcon} ${(Math.random() * 4 + 6).toFixed(1)}`;
+
+      // Add h4 to new a element
+      newTitleAElement.appendChild(newH4Element);
+
+      newTitleAElement.appendChild(newRatingDiv);
+
+      // Add new a element to list element
+      newLi.appendChild(newTitleAElement);
+   });
+};
 
 const allRadioButtons = document.getElementsByName("filter-buttons");
 
 allRadioButtons.forEach((filterButton) => {
-  filterButton.addEventListener("change", (event) => {
-    // let handleOnChangeEvent = event.target.value;
-
-    switch (event.target.value) {
-      case "recent-movies":
-        addMoviesToDom(moviesRecent);
-        break;
-      case "avengers-movies":
-        addMoviesToDom(moviesAvengers);
-        break;
-      case "xmen-movies":
-        addMoviesToDom(moviesXmen);
-        break;
-      case "princess-movies":
-        addMoviesToDom(moviesPrincess);
-        break;
-      case "batman-movies":
-        addMoviesToDom(moviesBatman);
-        break;
-      default:
-        console.log("Ehmm...");
-    }
-  });
+   filterButton.addEventListener("change", (event) => {
+      switch (event.target.value) {
+         case "all-movies":
+            addMoviesToDom(allMovies, "All Movies");
+            break;
+         case "recent-movies":
+            addMoviesToDom(moviesRecent, "Recent Movies");
+            break;
+         case "avengers-movies":
+            movieFilter("Avengers");
+            break;
+         case "xmen-movies":
+            movieFilter("X-Men");
+            break;
+         case "princess-movies":
+            movieFilter("Princess");
+            break;
+         case "batman-movies":
+            movieFilter("Batman");
+            break;
+         default:
+            console.log("Ehmm...");
+      }
+   });
 });
